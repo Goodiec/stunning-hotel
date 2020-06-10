221faadd8e565d44578c8d3c7b5401674fa99807
goodness chris-ugari <goodnesschrisugari@yahoo.com>
2020-06-10 12:07:20 +0100

    update

--- js/index.js
+++ js/index.js
@@ -14,3 +14,34 @@ let addActive = function(){
 addActive();
 
 //End Menu-header area function
+
+let toggleNavStatus = false;
+let menuToggleBtn = document.querySelector(".menu-btn");
+
+menuToggleBtn.addEventListener("click", function(){
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+
+    if(toggleNavStatus === false){
+        getSidebarUl.style.visibility = "visible";
+        getSidebar.style.width = "272px";
+        for(let i=0; i < arrayLength; i++){
+            getSidebarLinks[i].style.opacity = "1";
+        }
+        toggleNavStatus = true;
+    }
+    else if(toggleNavStatus === true){
+        getSidebar.style.width = "50px";
+
+        let arrayLength = getSidebarLinks.length;
+        for(let i=0; i < arrayLength; i++){
+            getSidebarLinks[i].style.opacity = "0";
+        }
+        getSidebarUl.style.visibility = "visible";
+        toggleNavStatus = false;
+    }
+
+}).classList.toggle('menu-btnc');
+
+
+    
